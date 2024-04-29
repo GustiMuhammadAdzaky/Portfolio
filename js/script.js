0921c20a1d0e937e81173e0dc5609f21370cc171
Zaky Gusti <181220091@unmuhpnk.ac.id>
2024-04-30 02:53:25 +0700

    fix navbar, jumbotron, grid

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ document.addEventListener("click", function (e) {
   }
 });
 
+// scroll effect
 window.addEventListener("scroll", function () {
   var navbar = document.querySelector(".navbar");
   if (window.scrollY > 100) {
@@ -24,3 +25,61 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+
+// filter content
+document.addEventListener("DOMContentLoaded", function (e) {
+  const filterButtons = document.querySelectorAll(".filter-btn");
+  const items = document.querySelectorAll(".item");
+
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const filter = button.dataset.filter;
+      // Hide all items
+      items.forEach((item) => {
+        item.style.display = "none";
+      });
+
+      // Show items based on selected filter
+      if (filter === "all") {
+        items.forEach((item) => {
+          item.style.display = "block";
+        });
+      } else {
+        const filteredItems = document.querySelectorAll("." + filter);
+        filteredItems.forEach((item) => {
+          item.style.display = "block";
+        });
+      }
+    });
+  });
+});
+
+// Event listener untuk tombol filter
+const filterButtons = document.querySelectorAll(".filter-buttons .filter-btn");
+filterButtons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    // Hapus kelas "active" dari semua tombol filter
+    filterButtons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    // Tambahkan kembali kelas "active" pada tombol yang ditekan
+    e.target.classList.add("active");
+  });
+});
+
+// Event listener untuk menghapus kelas "active" ketika area di luar tombol filter ditekan
+document.addEventListener("click", function (e) {
+  const filterButtonsContainer = document.querySelector(".filter-buttons");
+  const targetElement = e.target;
+
+  // Periksa apakah elemen yang ditekan bukan bagian dari .filter-buttons
+  if (!filterButtonsContainer.contains(targetElement)) {
+    // Hapus kelas "active" dari semua tombol filter
+    filterButtons.forEach((button) => {
+      button.classList.remove("active");
+    });
+  }
+});
+
+// console.log(filterButtons);
